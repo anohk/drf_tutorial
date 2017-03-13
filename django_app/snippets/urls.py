184bb28b1ed0e4c8e859edb7333766d8cda73bd1
anohk <anyoung.noh@gmail.com>
2017-03-13 22:42:50 +0900

    add endpoint to UserModel

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -8,6 +8,8 @@ app_name = 'snippets'
 urlpatterns = [
     url(r'^snippets/$', views.SnippetList.as_view(), name='snippet_list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet_detail'),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
