29220f62ca02c6facfeba41a390e86e559894440
anohk <anyoung.noh@gmail.com>
2017-03-13 21:02:25 +0900

    url 매핑

--- django_app/snippets/urls.py
+++ django_app/snippets/urls.py
@@ -1,7 +1,9 @@
-# from django.conf.urls import url
-#
-# app_name = 'snippets'
-#
-# urlpatterns = [
-#
-# ]
+from django.conf.urls import url
+from snippets import views
+
+app_name = 'snippets'
+
+urlpatterns = [
+    url(r'^snippets/$', views.snippet_list, name='snippet_list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detial, name='snippet_detail'),
+]
